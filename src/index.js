1caaae7f9515bfe0a03fa44f547d346641d6ca7c
Zheka90by <Lodbrok9@gmail.comm>
2021-07-23 01:33:10 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports =
+  function check(str, bracketsConfig) {
+    const arr = bracketsConfig.map(el => el.join(''))
+    for (let i = 0; i < arr.length; i++) {
+      if (str.includes(arr[i])) {
+        str = str.replace(arr[i], '')
+        i = -1
+      }
+    }
+    return str.length === 0
+  }
+
+
